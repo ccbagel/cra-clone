4dd2550bb5d152ca599d113bbe27655efd9fd131
ccbagel <hasan.abdi.ahmed97@gmail.com>
2021-12-02 16:06:56 -0700

    updating changes

---
+++ template/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
+registerServiceWorker();
+
+// register service worker
+function registerServiceWorker() {
+    if('serviceWorker' in navigator) {
+        navigator.serviceWorker.register("service-worker.js")
+            .then(() => {
+                console.log("service worker successfully registered");
+            })
+            .catch(err => {
+                console.log(err, err.message)
+            })
+    }
+}
+
+// unregister service worker
+function unregisterServiceWorker() {
+    navigator.serviceWorker.getRegistrations().then(registrations => {
+        registrations.forEach(reg => reg.unregister());
+    })
+} 
